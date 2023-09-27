0cd5e3b1c319c494e2a520d50315cc4e2de5ac55
Zeimoto <51506627+Zeimoto@users.noreply.github.com>
2023-09-27 14:53:51 +0100

    commit

--- province_s.py
+++
@@ -1,8 +0,0 @@
-import asyncio #to run the assyncronous processis
-import json
-import re
-from typing import Dict, List
-
-import httpx
-from parsel import Selector
-from typing_extensions import TypedDict
